11fab2d745e6bf2b4913afb903ec3f5f4196c4c2
kag@optim-al.com <kag@optim-al.com>
2023-11-27 12:47:03 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,11 +15,17 @@ class Form1(Form1Template):
     content_panel.addEventListener('touchstart', self.on_touch_start)
     content_panel.addEventListener('touchmove', self.on_touch_move)
     content_panel.addEventListener('touchend', self.on_touch_end)
-
+  
     self.touch_start_y = None
     self.touch_end_y = None
+    
+    # If there is a poll timer
+    #if Globals.is_pwa and Globals.is_mobile:
+      #self.timer_1.interval = None
 
-
+  def refresh_data(self, **event_args):
+    ## self.updatestuff_func()
+    pass
   def is_pwa_on_mobile(self,**kwargs):
     is_pwa = anvil.js.window.matchMedia("(display-mode: standalone)").matches
     is_mobile = anvil.js.window.navigator.userAgent.lower().find("mobi") > -1
@@ -29,10 +35,25 @@ class Form1(Form1Template):
 
 
   # Bunch of handlers
+  def on_touch_start(self, event):
+    self.touch_start_y = event.touches[0].clientY
+
+  def on_touch_move(self, event):
+    self.touch_end_y = event.touches[0].clientY
+
+  def on_touch_end(self, event):
+    if self.touch_start_y is not None and self.touch_end_y is not None:
+      pull_distance = self.touch_end_y - self.touch_start_y
+      if pull_distance > 50:
+        self.refresh_data()
+    self.touch_start_y = None
+    self.touch_end_y = None
+
+  '''My handlers 
   def on_touch_start(self, **event_args):
     pass
   def on_touch_move(self, **event_args):
     pass
   def on_touch_end(self, **event_args):
     pass
-    
+  '''
