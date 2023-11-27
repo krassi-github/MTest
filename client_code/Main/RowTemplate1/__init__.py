87c011a1cc828556993617eaede5f3061a15a8b5
kag@optim-al.com <kag@optim-al.com>
2023-11-27 15:50:13 +0000

    Edited Main.RowTemplate1

--- client_code/Main/RowTemplate1/__init__.py
+++ client_code/Main/RowTemplate1/__init__.py
@@ -5,5 +5,6 @@ class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
+    self.name.text = "T_name"  #self.item["name"]
+    self.m.text = "T_m"        #self.item[""]
