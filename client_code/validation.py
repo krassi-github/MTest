2648ecfa0730ad566f5c57c60da900ac13225a8d
kag@optim-al.com <kag@optim-al.com>
2023-11-29 07:22:46 +0000

    Edited validation

--- client_code/validation.py
+++ client_code/validation.py
@@ -1,4 +1,17 @@
 import anvil.server
+import re
+
+def validate_time(time_str):
+    # Regular expression pattern for HH:MM and HH:MM:SS formats
+    time_pattern = r'^([0-1][0-9]|2[0-3]):([0-5][0-9])(:([0-5][0-9]))?$'
+
+    # Check if the provided time string matches the pattern
+    if re.match(time_pattern, time_str):
+        print(f"The time '{time_str}' is valid.")
+        return True
+    else:
+        print(f"The time '{time_str}' is not in a valid HH:MM or HH:MM:SS format.")
+        return False
 
 
 class Validator():
