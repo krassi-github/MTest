e9b216f8486445cec75cca39014ece20b8333b68
kag@optim-al.com <kag@optim-al.com>
2023-12-27 15:14:05 +0000

    Edited Main.Change.ItemTemplate1

--- client_code/Main/Change/ItemTemplate1/__init__.py
+++ client_code/Main/Change/ItemTemplate1/__init__.py
@@ -13,3 +13,7 @@ class ItemTemplate1(ItemTemplate1Template):
     self.dr_med.text = self.item["rd_name"]
     self.dr_qty.text = self.item['rd_weight']
     self.dr_pcs.text = self.item['rd_pcs']
+
+  def link_1_click(self, **event_args):
+    alert(content="ready to SORT")
+    pass
