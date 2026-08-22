e8738c52903f4a98a58c904d0f3fc690bc467a32
kag@optim-al.com <kag@optim-al.com>
2026-08-22 18:48:19 +0000

    Created Intestine

---
+++ client_code/Intestine/__init__.py
@@ -0,0 +1,18 @@
+from ._anvil_designer import IntestineTemplate
+from anvil import *
+import anvil.server
+
+class Intestine(IntestineTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    super().__init__(**properties)
+
+    # Any code you write here will run before the form opens.
+
+
+
+
+
+
+
+# self.b_room_b.role = "elevated-button"
