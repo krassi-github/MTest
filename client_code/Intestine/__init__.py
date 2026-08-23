a3eb462cf6e85fc7b04f9af4f37aa01f0a6a9fdd
kag@optim-al.com <kag@optim-al.com>
2026-08-23 12:10:19 +0000

    Edited Intestine

--- client_code/Intestine/__init__.py
+++ client_code/Intestine/__init__.py
@@ -2,12 +2,20 @@ from ._anvil_designer import IntestineTemplate
 from anvil import *
 import anvil.server
 
+import datetime
+
 class Intestine(IntestineTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     super().__init__(**properties)
 
-    # Any code you write here will run before the form opens.
+    self.date_picker_1.format = "%Y/%m/%d %H:%M"
+    if self.date_picker_1.date is None:
+      #self.date_picker_1.date = datetime.datetime.now()
+      self.date_picker_1.pick_time = True
+      self.date_picker_1.date = datetime.datetime.now()
+
+
 
 
 
@@ -16,3 +24,11 @@ class Intestine(IntestineTemplate):
 
 
 # self.b_room_b.role = "elevated-button"
+
+  @handle("date_picker_1", "show")
+  def date_picker_1_show(self, **event_args):
+    c = DatePicker(format="%d %m %Y")
+
+    # Set to a datetime.datetime
+    c.pick_time = True
+    c.date = datetime.datetime.now()
