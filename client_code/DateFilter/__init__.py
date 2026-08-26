0e88e61ea136e02cd3887f9502ad95f3c4de9303
kag@optim-al.com <kag@optim-al.com>
2026-08-26 18:05:12 +0000

    Edited settings and DateFilter

--- client_code/DateFilter/__init__.py
+++ client_code/DateFilter/__init__.py
@@ -8,4 +8,44 @@ class DateFilter(DateFilterTemplate):
     # Set Form properties and Data Bindings.
     super().__init__(**properties)
 
-    # Any code you write here will run before the form opens.
+    self.mode = "D"          # D / 7D / 30D / R
+    self.anchor_date = ...
+    self.tb = ...
+    self.te = ...
+    self.range_from = None
+    self.range_to = None
+
+  @handle("D", "clicked")
+  def D_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    pass  # Write Code Here
+
+  @handle("radio_button_2", "clicked")
+  def radio_button_2_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    pass  # Write Code Here
+
+  @handle("radio_button_3", "clicked")
+  def radio_button_3_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    pass  # Write Code Here
+
+  @handle("R", "clicked")
+  def R_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    pass  # Write Code Here
+
+  @handle("prev_btn", "click")
+  def prev_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass  # Write Code Here
+
+  @handle("next_btn", "click")
+  def next_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass  # Write Code Here
+
+  @handle("period_btn", "click")
+  def period_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass  # Write Code Here
